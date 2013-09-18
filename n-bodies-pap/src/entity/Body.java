d90835a362a7a175b6148d7fa72a36e37853170b
Richiard <shadow674@libero.it>
2013-09-18 09:13:37 +0200

    Aggiunti alcuni commenti per un eventuale documentazione

--- n-bodies-pap/src/entity/Body.java
+++ n-bodies-pap/src/entity/Body.java
@@ -1,14 +1,33 @@
 package entity;
+
 import support.Vector;
 
-
+/**
+ * Class Body. 
+ * Represent the body single entity. Each body has three private fields that stand for his properties:
+ * - Vector p, position vector given by the coordination x,y
+ * - Vector v, velocity vector
+ * - double mass, the value of the mass of the body(planet)  
+ * 
+ * @author Richiard Casadei, Marco Zaccheroni
+ * 
+ *
+ */
 public class Body { 
-	private Vector r; //position
+	private Vector p; //position
 	private Vector v; //velocity
 	private double mass; //mass
 	
-	public Body(Vector r, Vector v, double mass) {
-		this.r = r;
+	/**
+	 * Class Body constructor.
+	 * Initialize the properties of the body with the object passed as parameter.
+	 * 
+	 * @param p - Vector Object
+	 * @param v - Vector Object
+	 * @param mass - double value of the mass
+	 */
+	public Body(Vector p, Vector v, double mass) {
+		this.p = p;
 		this.v = v;
 		this.mass = mass; 
 	}
@@ -16,19 +35,19 @@ public class Body {
 	public void move(Vector f, double dt) { 
 		Vector a = f.times(1/mass);
 		v = v.plus(a.times(dt));
-		r = r.plus(v.times(dt));
+		p = p.plus(v.times(dt));
 	}
 	
 	public Vector forceFrom(Body that) {
 		double G = 6.67e-11;
-		Vector delta = that.r.minus(this.r);
+		Vector delta = that.p.minus(this.p);
 		double dist = delta.magnitude();
 		double F = (G * this.mass * that.mass) / (dist * dist); 
 		return delta.direction().times(F);
 	} 
 	
 	public String getPosition(){
-		return r.toString();
+		return p.toString();
 	}
 	
 	public String getVelocity(){
--- n-bodies-pap/src/main/Controller.java
+++ n-bodies-pap/src/main/Controller.java
@@ -5,8 +5,8 @@ import entity.Body;
 
 /**
  * Class Controller. 
- * Rappresent the entity that inizialize the universe and the bodies taking the data from the
- * entity Generator
+ * Represent the entity that initialize the universe and the bodies taking the necessary 
+ * data from the entity Generator.
  * 
  * @author Richiard Casadei, Marco Zaccheroni
  * 
@@ -17,6 +17,11 @@ public class Controller {
 	private Body[] allbodies;
 	private int n;
 	
+	/**
+	 * Class Controller constructor.
+	 * 
+	 * @param gen - Generator Object
+	 */
 	public Controller(Generator gen){
 		
 		n = gen.getNumberOfBodies();
@@ -34,6 +39,10 @@ public class Controller {
 		
 	}
 	
+	/**
+	 * Function print_body
+	 * Print all the informations about all the bodies presented in the allbodies array 
+	 */
 	void print_body(){
 		for(int i = 0; i<n; i++){
 			System.out.println(allbodies[i].getPosition() + " " + allbodies[i].getVelocity() + " " + allbodies[i].getMass() );
