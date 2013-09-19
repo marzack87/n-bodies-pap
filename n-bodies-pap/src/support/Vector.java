d47782a22f1452cb115feb865f78f328b9fd586c
Richiard <shadow674@libero.it>
2013-09-19 09:14:14 +0200

    Aggiunti alcuni commenti anche a Vector

--- n-bodies-pap/src/support/Vector.java
+++ n-bodies-pap/src/support/Vector.java
@@ -1,16 +1,33 @@
 package support;
 
+/**
+ * Class Vector. 
+ * Represent the vector carrier as the geometrical element.
+ * 
+ * @author Richiard Casadei, Marco Zaccheroni
+ */
 public class Vector { 
 
     private final int N;         // length of the vector
     private double[] data;       // array of vector's components
 
+    /**
+     * Class Vector constructor.
+     * 
+     * @param N - Length of the vector 
+     */
     // create the zero vector of length N
     public Vector(int N) {
         this.N = N;
         this.data = new double[N];
     }
-
+    
+    /**
+     * Class Vector constructor.
+     * It creates a vector from an array of data and it also create a copy of data[]
+     * 
+     * @param data -  Array of data 
+     */
     // create a vector from an array
     public Vector(double[] data) {
         N = data.length;
@@ -21,11 +38,24 @@ public class Vector {
             this.data[i] = data[i];
     }
 
+    /**
+     * Method lenght.
+     * It returns the length of a vector.
+     * 
+     * @return N - length of the vector
+     */
     // return the length of the vector
     public int length() {
         return N;
     }
 
+    /**
+     * Method dot.
+     * It calculates the inner product between my vector and the vector passed as param 
+     * 
+     * @param that - Second vector
+     * @return sum - Inner product
+     */
     // return the inner product of this Vector a and b
     public double dot(Vector that) {
         if (this.N != that.N) throw new RuntimeException("Dimensions don't agree");
@@ -35,11 +65,24 @@ public class Vector {
         return sum;
     }
 
+    /**
+     * Method magnitude.
+     * It returns the Euclidean norm of a vector.
+     * So it assigns to a vector of a vectorial space, except the zero, a positive length.
+     * 
+     * @return norm - The vector length
+     */
     // return the Euclidean norm of this Vector
     public double magnitude() {
         return Math.sqrt(this.dot(this));
     }
-
+    
+    /**
+     * Method magnitude.
+     * It returns the Euclidean distance between two vectors.
+     * 
+     * @return dist - The distance
+     */
     // return the Euclidean distance between this and that
     public double distanceTo(Vector that) {
         if (this.N != that.N) throw new RuntimeException("Dimensions don't agree");
