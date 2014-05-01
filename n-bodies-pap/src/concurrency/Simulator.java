d80c7aaed8055aa88498d6a438d3a43a133c7720
richiardcasadei <richiardcasadei@192.168.0.2>
2014-05-01 12:28:33 +0200

    loopv4 sembra una bomba ahahah

--- n-bodies-pap/src/concurrency/Simulator.java
+++ n-bodies-pap/src/concurrency/Simulator.java
@@ -7,6 +7,7 @@ import java.util.concurrent.*;
 import entity.Body;
 import support.Context;
 import support.Util;
+import support.V2d;
 
 public class Simulator extends Thread {
 	
@@ -41,7 +42,8 @@ public class Simulator extends Thread {
 					//double t0 = System.nanoTime();
 					//loop();
 					//loopV2();
-					loopV3();
+					//loopV3();
+					loopV4();
 					//double t1 = System.nanoTime();
 					//log("Task execution time: " + (t1-t0));
 			}
@@ -193,6 +195,66 @@ public class Simulator extends Thread {
 			e.printStackTrace();
 		}
 	}
+	
+	private void loopV4(){
+		/*
+		 * - MASTER&WORKER PATTERN
+		 * - FACCIO TUTTO DENTRO RUN() SENZA CHIAMARE I METODI
+		 * - POI SARA' IL MASTER AD AGGIORNARE IL BODY NEL CONTEST ED EVENTUALMENTE A GESTIRE LE COLLISIONI
+		 * 	 RISCONTRATE DAI WORKER
+		 */
+		double time = System.nanoTime();
+		
+		Body [] all_bodies = context.allbodies.clone();
+		double dt = context.dt;
+		
+		int n = NTHREADS/2;
+		int level = Math.round((float)(Math.log(n)/Math.log(2))) + 1;
+		double delta_t = context.dt;
+		
+		for (int i = 0; i < all_bodies.length; i++) {
+			Body me = all_bodies[i];
+			V2d force_dest = new V2d(0,0);
+	     	SimulatorWorker master = new SimulatorWorker(all_bodies, me, 0, all_bodies.length, level, force_dest, delta_t);
+	     	try {
+				master.join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			//if(me.getMassValue() != Util.SUN_MASS)me.move(force, dt);
+			if(me.getMassValue() != Util.SUN_MASS){
+				if (me.collision){
+					me.v = me.vel_after_collision;
+					//System.out.println("Body " + index + " - vel_after_collision = " + vel_after_collision);
+					V2d dp = me.v.mul(dt);
+					me.p = me.p.sum(dp);
+				} else {
+					V2d a = force_dest.mul(1/me.mass);
+					V2d dv = a.mul(dt);
+					V2d dp = (me.v.sum(dv.mul(1/2))).mul(dt);
+					me.p = me.p.sum(dp);
+					me.v = me.v.sum(dv);
+				}
+				
+				me.collision = false;
+				me.vel_after_collision.x = 0;
+				me.vel_after_collision.y = 0;	
+			}
+			
+			context.updateBody(me);
+				
+		}
+		
+		Util.total_iteration++;
+		Util.last_iter_time = (System.nanoTime() - time)*1e-9;
+		this.sem.release();
+		try {
+			this.printed.acquire();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
 	
 	/**
      * Private method log.
---
+++ n-bodies-pap/src/concurrency/SimulatorWorker.java
@@ -0,0 +1,102 @@
+package concurrency;
+
+import java.util.concurrent.Callable;
+
+import support.Context;
+import support.Util;
+import support.V2d;
+import entity.Body;
+
+public class SimulatorWorker extends Thread {
+	
+	private final Body[] all_bodies;
+	private final int my_index;
+	private Body me;
+	private int middle; 
+	private int from;
+	private int to;
+	private int level;
+	private V2d force_dest;
+	private double delta_t;
+	
+	private double dt;
+	
+	/**
+	 * Class BodyTaskV4 default constructor.
+	 *
+	 **/
+	public SimulatorWorker(Body[] all, Body body, int from, int to, int level, V2d force_dest, double delta_t){
+		all_bodies = all;
+		me = body;
+		my_index = body.getIndex();
+		dt = delta_t;
+		this.from = from;
+		this.to = to;
+		this.level = level;
+		this.force_dest = force_dest;
+		this.delta_t = delta_t;
+	}
+	
+	public void run() {
+		
+		
+		if(level > 0){
+			middle = (from+to)/2;
+			
+			SimulatorWorker work1 = new SimulatorWorker(all_bodies, me, from, middle, level-1, force_dest, delta_t);
+			SimulatorWorker work2 = new SimulatorWorker(all_bodies, me, middle, to, level-1, force_dest, delta_t);
+			work1.start();
+			work2.start();
+			
+			try {
+				work1.join();
+				work2.join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+		}else{
+			
+			V2d force = new V2d(0,0);
+			
+			for (; from < to; from++) {
+				if (from != my_index) {
+					//force = force.sum(me.forceFrom(all_bodies[i]));
+					double G = 6.67;
+					Body that = all_bodies[from];
+					V2d p_this = new V2d(me.p.x, me.p.y);
+					V2d p_that = new V2d(that.p.x, that.p.y);
+					V2d delta = p_that.min(p_this);
+					double dist = that.p.dist(me.p);
+					
+					if (that.getMassValue() == Util.SUN_MASS){
+						if (dist*Util.scaleFact <= (Util.SUN_RADIUS + Util.BODY_RADIUS)){
+							//collision(that);
+							me.collision = true;
+							
+							V2d final_v = me.v.mul(me.mass - that.mass).sum(that.v.mul(2 * that.mass)).mul(1 / (me.mass + that.mass));
+							me.vel_after_collision = me.vel_after_collision.sum(final_v);
+						}
+					} else {
+						if (dist*Util.scaleFact <= (Util.BODY_RADIUS * 2)) {
+							me.collision = true;
+							
+							V2d final_v = me.v.mul(me.mass - that.mass).sum(that.v.mul(2 * that.mass)).mul(1 / (me.mass + that.mass));
+							me.vel_after_collision = me.vel_after_collision.sum(final_v);
+						
+						}
+					}
+					
+					double F = (G * (me.mass) * (that.mass)) / (dist * dist);
+					
+					V2d delta_normalized = delta.getNormalized();
+					force = delta_normalized.mul(F);
+					
+					force_dest.sum(force);
+				}
+			}
+		}
+		
+	}
+
+}
