8b3f7ca3f559a174cbbcae0d5d5d461f43c7b6a7
richiardcasadei <richiardcasadei@192.168.36.234>
2014-07-18 17:47:57 +0200

    gif

--- n-bodies-pap/src/gui/DoneDialog.java
+++ n-bodies-pap/src/gui/DoneDialog.java
@@ -1,7 +1,10 @@
 package gui;
 
 import java.awt.*;
+
 import javax.swing.*;
+
+import support.Util;
 
 import java.awt.event.*;
 
@@ -24,20 +27,54 @@ public class DoneDialog extends JDialog implements ActionListener{
 	public DoneDialog(){
 		
 		super();
-		setSize(new Dimension(350, 70));
-		setLayout(new BorderLayout());
-		JLabel s = new JLabel("Your simulation stats was successfully saved.");
-		add(s,BorderLayout.CENTER);
-		s.setHorizontalAlignment(JLabel.CENTER);
-		JButton well = new JButton("Well done!");
-		well.addActionListener(this);
-		add(well,BorderLayout.SOUTH);
-		setLocation((Toolkit.getDefaultToolkit().getScreenSize().width - this.getWidth()) / 2, (Toolkit.getDefaultToolkit().getScreenSize().height - this.getHeight()) / 2);
-		
+		if(Util.star_wars_theme){
+			setSize(new Dimension(350, 300));
+			setLayout(new BorderLayout());
+			JLabel s = new JLabel("Your simulation stats was successfully saved.");
+			add(s,BorderLayout.NORTH);
+			s.setHorizontalAlignment(JLabel.CENTER);
+			
+			ImageIcon logo = createImageIcon("images/darth.gif", "Dance");
+			JLabel logosw = new JLabel(logo);
+			add(logosw,BorderLayout.CENTER);
+			
+			JButton well = new JButton("Well done!");
+			well.addActionListener(this);
+			add(well,BorderLayout.SOUTH);
+			setLocation((Toolkit.getDefaultToolkit().getScreenSize().width - this.getWidth()) / 2, (Toolkit.getDefaultToolkit().getScreenSize().height - this.getHeight()) / 2);
+		}else{
+			setSize(new Dimension(350, 70));
+			setLayout(new BorderLayout());
+			JLabel s = new JLabel("Your simulation stats was successfully saved.");
+			add(s,BorderLayout.CENTER);
+			s.setHorizontalAlignment(JLabel.CENTER);
+			JButton well = new JButton("Well done!");
+			well.addActionListener(this);
+			add(well,BorderLayout.SOUTH);
+			setLocation((Toolkit.getDefaultToolkit().getScreenSize().width - this.getWidth()) / 2, (Toolkit.getDefaultToolkit().getScreenSize().height - this.getHeight()) / 2);
+		}
 	}
 	
 	public void actionPerformed(ActionEvent e){
 		System.exit(0);
+	}
+	
+	/** 
+	 * Private method createImageIcon.
+	 * <p>
+	 * Returns an ImageIcon, or null if the path was invalid.
+	 * 
+	 * @param path The path of the image file 
+	 * @param description Description of the image
+	 */
+	private ImageIcon createImageIcon(String path,String description) {
+	    java.net.URL imgURL = getClass().getResource(path);
+	    if (imgURL != null) {
+	        return new ImageIcon(imgURL, description);
+	    } else {
+	        System.err.println("Couldn't find file: " + path);
+	        return null;
+	    }
 	}
 
 }
